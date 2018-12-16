972620e7c08a5df90d56258d520f19887cb80310
xtrp <36894883+xtrp@users.noreply.github.com>
2018-12-16 13:01:52 +0000

    updates

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,6 +1,5 @@
 var sketchColor = "#000000";
 
-
 $(function(){
   var sketchColorPicker = $('#colorPicker');
   sketchColorPicker.tinycolorpicker();
@@ -9,3 +8,7 @@ $(function(){
     sketchColor = (sketchColorPicker.data("plugin_tinycolorpicker").colorHex);
   });
 });
+
+var canvas = document.querySelector("div.container > canvas");
+canvas.width = canvas.offsetWidth;
+canvas.height = (canvas.width / 4) * 3;
